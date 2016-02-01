70ccec3a3d4abc71affc5e255912aeb78645aef8
pkx0128 <764670547@qq.com>
2016-02-01 16:05:07 +0800

    add project

---
+++ tongjizhushou/tongjizhushou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tongjizhushou
+//
+//  Created by Pankx on 16/2/1.
+//  Copyright © 2016年 Pankx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
